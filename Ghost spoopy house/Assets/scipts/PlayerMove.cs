dbf2645e73b0dfddce525219f6d99c2b7c27b8c0
Unknown <blackychan5522@gmail.com>
2017-10-25 13:10:37 -0600

    Added more code

---
+++ Ghost spoopy house/Assets/scipts/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+	public float moveSpeed;
+	public float turnSpeed;
+	public float jumpHight;
+	
+
+	void Update () {
+		var j = Input.GetAxis("Jump")* Time.deltaTime * jumpHight;
+		var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
+		var z = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
+
+		transform.Rotate(0,y,0);
+		transform.Translate(0,0,z);
+		transform.Translate(0,j,0);
+	 
+	
+	}
+}
---
+++ Ghost spoopy house/Assets/scipts/flashlight.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class flashlight : MonoBehaviour {
+ 
+ public bool lightOn;
+
+ Light light;
+
+ void Start() {
+	 light = GetComponent<Light> ();
+//set light default on
+	 lightOn = true;
+	 light.enabled = true;
+ }
+
+		//Update is called once per frame
+		void Update() {
+			//Toggle light on/off when L key is pressed
+			else if (Input.GetKeyUp (KeyCode.F)&& !lightOn )) {
+				lightOn= true;
+				light.enabled = true;
+
+			}
+
+		}
+
+	 
+	 }
+ }
+	
+	
+
